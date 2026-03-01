df4b3c006ca292f03e60ea1391fbbdd248751325
jfqd <jfqd@blun.org>
2026-03-01 22:52:56 +0100

    more fixes on the way

--- copy/var/zoneinit/includes/42-php-fpm.sh
+++ copy/var/zoneinit/includes/42-php-fpm.sh
@@ -3,7 +3,7 @@
 # Configure PHP sendmail return-path if possible
 if mdata-get kumquat_admin_email 1>/dev/null 2>&1; then
   echo "php_admin_value[sendmail_path] = /usr/sbin/sendmail -t -i -f $(mdata-get kumquat_admin_email)" \
-    >> /opt/local/etc/php/8.3/php-fpm.d/pool-www.conf
+    >> /opt/local/etc/php/8.3/php-fpm.d/www.conf
 fi
 
 # Enable PHP-FPM
