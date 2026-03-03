a70af6daf00f85ec79a9889bb113e9c82ea0c09e
jfqd <jfqd@blun.org>
2026-03-03 14:26:33 +0100

    fix logadm for php83

--- copy/var/zoneinit/includes/61-logadm.sh
+++ copy/var/zoneinit/includes/61-logadm.sh
@@ -10,5 +10,5 @@ logadm -w '/var/log/proftpd/proftpd.log' -A 7d -p 1d -c -N -m 640
 logadm -w '/var/log/proftpd/xferlog'     -A 7d -p 1d -c -N -m 640
 
 # php
-logadm -w '/var/log/php-fpm.log'         -A 7d -p 1d -c -N -m 640
+logadm -w '/var/log/php-fpm83.log'       -A 7d -p 1d -c -N -m 640
 
